b768bb1dccaee4c50b68c246978fa7c53fd50c7b
teichhorst <taylor.eichhorst@gmail.com>
2013-10-29 08:20:24 -0600

    Started Project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webgladiator::Application.config.secret_token = '5cdaf0ae9d968db245e31082de6de455b3e90ccdc29030febaa90def62d54c64545c2c1bae8967fa3cb8cf5cbd96119f4eea80a89ad195d773274305708a81e2'
